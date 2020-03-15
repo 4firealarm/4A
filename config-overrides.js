2a31fa59a869d6a0aac80f620e6041e1478e473c
Pony <mycnj123@gmail.com>
2020-03-15 21:59:31 +0800

    init

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true
+  }),
+  // 主题自定义变量
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { '@primary-color': '#1DA57A','@black':'#224350' },
+  }),
+);
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Button } from 'antd'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{ fontSize:32 }}>Hello World</div>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
